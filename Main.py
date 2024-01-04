32e5a34fb7d016f6c8b1dfab33f8a3bc0aa445f5
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-04 19:45:08 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
+from keras.models import load_model
+
+custom_objects = {"YourCustomLayer": YourCustomLayer}
+model = load_model('Stock_Predictions_Model.keras', custom_objects=custom_objects)
+
+
 st.write("Hello **world**!")
