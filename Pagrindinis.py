460fb14d4a20c2bad78791717b8e55fdb86c47c4
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-06 18:21:45 +0200

    Update Pagrindinis.py

--- Pagrindinis.py
+++ Pagrindinis.py
@@ -1,61 +1,63 @@
 import streamlit as st
 import numpy as np
+import datetime
 import webbrowser
+import os
+from streamlit_lottie import st_lottie
+from PIL import Image
+import requests
+import json
+
+st.image("logo2.png")
+
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("style/style.css")
+
+def load_lottiefile(filepath: str):
+   with open(filepath, "r") as f:
+      return json.load(f)
+
+lottie_coding = load_lottiefile("lottiefile/coding.json")
+
+st.title("")
+st_lottie(
+   lottie_coding,
+   speed=1,
+   reverse=True,
+   loop=True,
+)
 
 
 
+    
 def main():
  if st.sidebar.button("Susisiekti"):
-   st.sidebar.markdown("[Visit instagram🎨](https://www.instagram.com/egidijauss/)")
-   st.sidebar.markdown("[Visit Youtube💖](https://www.youtube.com/channel/UC3_-vsk8JO05rVE_dQWjJFQ)")
-   st.sidebar.markdown("[Visit Facebook🧢](https://www.facebook.com/EgiFoto)")
+   st.sidebar.markdown("[🎨Visit instagram](https://www.instagram.com/egidijauss/)")
+   st.sidebar.markdown("[💖Visit Youtube](https://www.youtube.com/channel/UC3_-vsk8JO05rVE_dQWjJFQ)")
+   st.sidebar.markdown("[🧢Visit Facebook](https://www.facebook.com/EgiFoto)")
    st.sidebar.markdown("")
 if __name__ == "__main__":
     main()
 
 
-   
-
-st.image("logo.png")
-st.image("tuscia.png")
-st.title(" ")
-st.write(" ")
-
-st.image("1.jpg",)
-st.image("2.jpg")
-st.image("3.jpg")
-st.image("4.jpg")
-st.image("5.jpg")
-st.image("6.jpg")
-st.image("7.jpg")
-st.image("8.jpg")
-st.image("9.jpg")
-st.image("10.jpg")
-st.image("11.jpg")
-st.image("12.jpg")
-st.image("13.jpg",)
-st.image("14.jpg")
-st.image("15.jpg")
-st.image("16.jpg")
-st.image("17.jpg")
-st.image("18.jpg")
-st.image("19.jpg")
-st.image("20.jpg")
-st.image("21.jpg")
-st.image("22.jpg")
-st.image("23.jpg")
-st.image("24.jpg")
-
-st.image('1.jpg', caption='Sunrise by the mountains')
 
 
+# Path to the folder containing images
+image_folder = "folder1"
 
-   
-   
+# List all files in the folder
+image_files = os.listdir(image_folder)
 
+# Display each image in the folder
+for image_file in image_files:
+    # Construct the full path to the image file
+    image_path = os.path.join(image_folder, image_file)
 
-
-   
+    # Display the image
+    st.image(image_path, use_column_width=True)
 
 
 
