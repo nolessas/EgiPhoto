c92f233f4138ba6ac3bc2013f790c765c6e4b7ba
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-06 21:43:26 +0200

    Update Pagrindinis.py

--- Pagrindinis.py
+++ Pagrindinis.py
@@ -50,26 +50,3 @@ if __name__ == "__main__":
 
 
 
-
-# Path to the folder containing images
-image_folder = "folder1"
-
-# List all files in the folder and sort them naturally
-image_files = natsorted(os.listdir(image_folder))
-
-# Display each image in the folder
-for image_file in image_files:
-    # Construct the full path to the image file
-    image_path = os.path.join(image_folder, image_file)
-
-    # Display the image
-    st.image(image_path, use_column_width=True)
-
-# Read and display a specific image
-with open("figures/cigar.jpeg", 'rb') as f:
-    image = f.read()
-
-image_bytes = base64.b64encode(image).decode()
-local_file = f'<p style="text-align:center;"><img src="data:image/jpeg;base64,{image_bytes}" alt="Image" width=300></p>'
-
-st.markdown(local_file, unsafe_allow_html=True)
