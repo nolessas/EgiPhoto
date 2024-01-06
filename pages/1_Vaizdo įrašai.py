b750f1c9de1ab8f016828bad97c915795268dbb9
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-06 22:24:14 +0200

    Update 1_Vaizdo įrašai.py

--- pages/1_Vaizdo įrašai.py
+++ pages/1_Vaizdo įrašai.py
@@ -4,6 +4,16 @@ import os
 def main():
     st.title("Smagaus žiūrėjimo!")
 
+    def main():
+ if st.sidebar.button("Susisiekti"):
+   st.sidebar.markdown("[🎨Visit instagram](https://www.instagram.com/egidijauss/)")
+   st.sidebar.markdown("[💖Visit Youtube](https://www.youtube.com/channel/UC3_-vsk8JO05rVE_dQWjJFQ)")
+   st.sidebar.markdown("[🧢Visit Facebook](https://www.facebook.com/EgiFoto)")
+   st.sidebar.markdown("")
+if __name__ == "__main__":
+    main()
+
+
     # Display the video player
     st.video("https://www.youtube.com/watch?v=Tn-KRogA23g")
     st.video("https://www.youtube.com/watch?v=Jt7c8B0bJUE")
