5fe27e5979854fa0b7439c2b2c5096d0c3a9f819
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-06 19:09:23 +0200

    Update Pagrindinis.py

--- Pagrindinis.py
+++ Pagrindinis.py
@@ -6,29 +6,6 @@ import json
 from natsort import natsorted
 
 st.image("logo2.png")
-
-# Use local CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-local_css("style/style.css")
-
-def load_lottiefile(filepath: str):
-   with open(filepath, "r") as f:
-      return json.load(f)
-
-lottie_coding = load_lottiefile("lottiefile/coding.json")
-
-st.title("")
-st_lottie(
-   lottie_coding,
-   speed=1,
-   reverse=True,
-   loop=True,
-)
-
-
-
     
 def main():
  if st.sidebar.button("Susisiekti"):
@@ -40,10 +17,6 @@ if __name__ == "__main__":
     main()
 
 
-
-
-import streamlit as st
-import os
 from natsort import natsorted
 
 # Path to the folder containing images
