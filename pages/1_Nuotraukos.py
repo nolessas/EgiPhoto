df376561f17ace2397ffd46615d391b9170c9de7
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-06 18:51:43 +0200

    Add files via upload

---
+++ pages/1_Nuotraukos.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import numpy as np
+
+
+    
+def main():
+ if st.sidebar.button("Susisiekti"):
+   st.sidebar.markdown("[🎨Visit instagram](https://www.instagram.com/egidijauss/)")
+   st.sidebar.markdown("[💖Visit Youtube](https://www.youtube.com/channel/UC3_-vsk8JO05rVE_dQWjJFQ)")
+   st.sidebar.markdown("[🧢Visit Facebook](https://www.facebook.com/EgiFoto)")
+   st.sidebar.markdown("")
+if __name__ == "__main__":
+    main()
+
+
+
+
+# Path to the folder containing images
+image_folder = "folder1"
+
+# List all files in the folder
+image_files = os.listdir(image_folder)
+
+# Display each image in the folder
+for image_file in image_files:
+    # Construct the full path to the image file
+    image_path = os.path.join(image_folder, image_file)
+
+    # Display the image
+    st.image(image_path, use_column_width=True)
+
+
+
