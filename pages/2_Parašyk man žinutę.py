401fd24f07fc60b448d40eb18f360235cd31f369
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-06 22:24:25 +0200

    Update 2_Parašyk man žinutę.py

--- pages/2_Parašyk man žinutę.py
+++ pages/2_Parašyk man žinutę.py
@@ -3,6 +3,16 @@ import numpy as np
 import webbrowser
 import os
 
+def main():
+ if st.sidebar.button("Susisiekti"):
+   st.sidebar.markdown("[🎨Visit instagram](https://www.instagram.com/egidijauss/)")
+   st.sidebar.markdown("[💖Visit Youtube](https://www.youtube.com/channel/UC3_-vsk8JO05rVE_dQWjJFQ)")
+   st.sidebar.markdown("[🧢Visit Facebook](https://www.facebook.com/EgiFoto)")
+   st.sidebar.markdown("")
+if __name__ == "__main__":
+    main()
+
+
 
 # Use local CSS
 def local_css(file_name):
