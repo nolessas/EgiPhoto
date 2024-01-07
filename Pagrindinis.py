0d1361f734612dcd815b71cc0e06e566314de77a
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-07 02:18:34 +0200

    Update Pagrindinis.py

--- Pagrindinis.py
+++ Pagrindinis.py
@@ -43,8 +43,9 @@ def display_nuotraukos():
         # Construct the full path to the image file
         image_path = os.path.join(image_folder, image_file)
 
-        # Display the image with 10% width
-        st.image(image_path, width=0.1)
+        # Display the image with custom CSS styling
+        st.markdown(f'<img src="{image_path}" style="width: 10%;">', unsafe_allow_html=True)
+
 
 
 
